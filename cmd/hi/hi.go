acef3629d6afcd7b8da45d2f0c21349a6308a568
Paul Miller <paul@jettero.pl>
2022-02-05 11:34:41 -0500

    this sorta works for '.' 'coal', …
    … but nothing else can match cuz the line is all fubar with coal
    marks. I think I originally did this with a pushdown stack
    describing the color status

--- cmd/hi/hi.go
+++ cmd/hi/hi.go
@@ -5,21 +5,48 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"regexp"
 )
+
+type pattern struct {
+	pattern string
+	color   string
+	matcher *regexp.Regexp
+}
+
+func printLine(patterns []pattern, line string) {
+	for i := 0; i < len(patterns); i++ {
+		fmt.Printf("pat: %v\n", patterns[i])
+		line = patterns[i].matcher.ReplaceAllStringFunc(line, func(m string) string {
+			return fmt.Sprintf("[%s]%s[/%s]", patterns[i].color, m, patterns[i].color)
+		})
+	}
+	fmt.Println(line)
+}
 
 // Execute runs the actual program (meant to be used in main.go)
 func Execute() {
 	reader := bufio.NewReader(os.Stdin)
+	var patterns []pattern
+
+	for i := 1; i < len(os.Args); i += 2 {
+		p := pattern{
+			pattern: os.Args[i],
+			matcher: regexp.MustCompile(os.Args[i]),
+			color:   os.Args[i+1],
+		}
+		patterns = append(patterns, p)
+	}
+
 	for true {
 		line, something := reader.ReadString('\n')
 
 		if something == io.EOF {
 			if len(line) > 0 {
-				fmt.Println(line)
+				printLine(patterns, line)
 			}
 			break
 		}
-
-		fmt.Println(line)
+		printLine(patterns, line)
 	}
 }
