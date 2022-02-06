f41cf16f4428accad935b986d66fac2f7e5f3818
Paul Miller <paul@jettero.pl>
2022-02-06 11:35:44 -0500

    I might need a terminal width at some point. not sure if this works.

---
+++ internal/terminal_size.go
@@ -0,0 +1,29 @@
+package termsz
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+type winsize struct {
+	Row    uint16
+	Col    uint16
+	Xpixel uint16
+	Ypixel uint16
+}
+
+// never tried or tested this code
+// ripped from
+// https://stackoverflow.com/a/16576712/388466
+func getWidth() uint {
+	ws := &winsize{}
+	retCode, _, errno := syscall.Syscall(syscall.SYS_IOCTL,
+		uintptr(syscall.Stdin),
+		uintptr(syscall.TIOCGWINSZ),
+		uintptr(unsafe.Pointer(ws)))
+
+	if int(retCode) == -1 {
+		panic(errno)
+	}
+	return uint(ws.Col)
+}
