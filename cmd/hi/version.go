d55356b5b49cc5e9833f23ec13e876f119a0ec4e
Paul Miller <paul@jettero.pl>
2022-02-16 23:00:52 -0500

    golang gives me the rage

--- cmd/hi/version.go
+++ cmd/hi/version.go
@@ -9,16 +9,24 @@ package main
 
 import (
 	"fmt"
+	"runtime/debug"
 )
 
 var (
-	Version    = "dev"
-	CommitHash = "n/a"
-	BuildTime  = "n/a"
+	Version    = "who fucking knows"
+	CommitHash = "there's no way to embed this in a go program without a makefile or something"
+	BuildTime  = "you got me man, use the makefile, go build sucks"
 )
 
 func PrintBuildVersion() {
 	fmt.Printf("Version:    %s\n", Version)
 	fmt.Printf("CommitHash: %s\n", CommitHash)
 	fmt.Printf("BuildTime:  %s\n", BuildTime)
+
+	if bi, ok := debug.ReadBuildInfo(); ok {
+		fmt.Printf("\n")
+		fmt.Printf("BuildInfo.Main.Path:    %s\n", (*bi).Main.Path)
+		fmt.Printf("BuildInfo.Main.Version: %s\n", (*bi).Main.Version)
+		fmt.Printf("BuildInfo.Main.Sum:     %s\n", (*bi).Main.Sum)
+	}
 }
