cc26b755579828ffa030cb4e68a75838243844db
Paul Miller <paul@jettero.pl>
2022-02-16 23:09:24 -0500

    Fuck I hate golang. There's no way to make 'go install' actually insert build info

--- cmd/hi/version.go
+++ cmd/hi/version.go
@@ -9,24 +9,36 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"runtime/debug"
 )
 
 var (
-	Version    = "who fucking knows"
-	CommitHash = "there's no way to embed this in a go program without a makefile or something"
-	BuildTime  = "you got me man, use the makefile, go build sucks"
+	Version    = ""
+	CommitHash = ""
+	BuildTime  = ""
 )
 
 func PrintBuildVersion() {
-	fmt.Printf("Version:    %s\n", Version)
-	fmt.Printf("CommitHash: %s\n", CommitHash)
-	fmt.Printf("BuildTime:  %s\n", BuildTime)
+	if Version == "" {
+		fmt.Printf("--version doesn't work if you 'go run' or 'go install'. Use 'make' instead.\n")
+		fmt.Printf("Here's some useless BuildInfo:\n")
+	} else {
+		fmt.Printf("Version:    %s\n", Version)
+		fmt.Printf("CommitHash: %s\n", CommitHash)
+		fmt.Printf("BuildTime:  %s\n", BuildTime)
+	}
 
 	if bi, ok := debug.ReadBuildInfo(); ok {
 		fmt.Printf("\n")
 		fmt.Printf("BuildInfo.Main.Path:    %s\n", (*bi).Main.Path)
 		fmt.Printf("BuildInfo.Main.Version: %s\n", (*bi).Main.Version)
 		fmt.Printf("BuildInfo.Main.Sum:     %s\n", (*bi).Main.Sum)
+	}
+
+	if Version == "" {
+		os.Exit(1)
+	} else {
+		os.Exit(0)
 	}
 }
