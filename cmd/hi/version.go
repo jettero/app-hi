08f3e3126515a9e398bef83746f862e3de57bd2a
Paul Miller <paul@jettero.pl>
2022-02-17 10:16:08 -0500

    this is a less angry version of the versioning

--- cmd/hi/version.go
+++ cmd/hi/version.go
@@ -9,7 +9,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"runtime/debug"
 )
 
@@ -20,25 +19,24 @@ var (
 )
 
 func PrintBuildVersion() {
-	if Version == "" {
-		fmt.Printf("--version doesn't work if you 'go run' or 'go install'. Use 'make' instead.\n")
-		fmt.Printf("Here's some useless BuildInfo:\n")
+	if Version != "" {
+		fmt.Printf("Makefile versioning:\n")
+		fmt.Printf("  Version:    %s\n", Version)
+		fmt.Printf("  CommitHash: %s\n", CommitHash)
+		fmt.Printf("  BuildTime:  %s\n", BuildTime)
+
 	} else {
-		fmt.Printf("Version:    %s\n", Version)
-		fmt.Printf("CommitHash: %s\n", CommitHash)
-		fmt.Printf("BuildTime:  %s\n", BuildTime)
+		fmt.Printf("Makefile versioning unavailable.\n")
 	}
 
-	if bi, ok := debug.ReadBuildInfo(); ok {
-		fmt.Printf("\n")
-		fmt.Printf("BuildInfo.Main.Path:    %s\n", (*bi).Main.Path)
-		fmt.Printf("BuildInfo.Main.Version: %s\n", (*bi).Main.Version)
-		fmt.Printf("BuildInfo.Main.Sum:     %s\n", (*bi).Main.Sum)
+	if bi, ok := debug.ReadBuildInfo(); ok && (*bi).Main.Version != "(devel)" {
+		fmt.Printf("Go Module Proxy versioning:\n")
+		fmt.Printf("  BuildInfo.Main.Path:    %s\n", (*bi).Main.Path)
+		fmt.Printf("  BuildInfo.Main.Version: %s\n", (*bi).Main.Version)
+		fmt.Printf("  BuildInfo.Main.Sum:     %s\n", (*bi).Main.Sum)
+
+	} else {
+		fmt.Printf("Go Module Proxy versioning unavailable.\n")
 	}
 
-	if Version == "" {
-		os.Exit(1)
-	} else {
-		os.Exit(0)
-	}
 }
