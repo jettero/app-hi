15a8eb5711e0af00a668b6f5526fabdb4a355a83
Paul Miller <paul@jettero.pl>
2022-08-19 10:05:40 -0400

    how did I miss FindStringSubmatch?

--- cruft/pcre.go
+++ cruft/pcre.go
@@ -15,13 +15,9 @@ func main() {
 		fmt.Println(err)
 		os.Exit(2)
 	}
-	matches := re.FindSubmatch([]byte("ababab"))
+	matches := re.FindStringSubmatch("ababab")
 	if len(matches) > 0 {
-		groups := make([]string, len(matches))
-		for index, match := range matches {
-			groups[index] = string(match)
-		}
-		fmt.Println("matched!", groups)
+		fmt.Println("matched!", matches)
 		os.Exit(0)
 	} else {
 		fmt.Println("not matched")
