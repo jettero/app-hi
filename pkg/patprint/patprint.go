b7790e668849430af64ac44f6589ba8a49a2b3af
Paul Miller <paul@jettero.pl>
2023-07-29 11:56:38 -0400

    split out MahCallback … so I can make some not-objects

--- pkg/patprint/patprint.go
+++ pkg/patprint/patprint.go
@@ -7,8 +7,6 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/jettero/app-hi/pkg/dfmt"
-
 	c "github.com/jettero/app-hi/pkg/colors"
 	"go.elara.ws/pcre"
 	// The built in regexp.* in Golang is absolutely awful.
@@ -77,46 +75,6 @@ func generateAnnotations(color string, all_matches [][]int) []annotation {
 		}
 	}
 	return ret
-}
-
-func matched(b bool) int32 {
-    if b {
-        dfmt.Printf(" match=yes\n")
-        return 0;
-    }
-    dfmt.Printf(" match=no\n")
-    return 1;
-}
-
-func MahCallback(cb *pcre.CalloutBlock) int32 {
-    // There's some DEBUG_HI Print items elsewhere in this file, but for
-    // callouts -- which could get executed frequently -- I chose to only
-    // enable them at compile time when needed
-
-    if cb.CalloutNumber == 0 {
-        s := strings.Split(cb.CalloutString, " ")
-        m := len(cb.Substrings)-1
-        if len(s) == 2 && m >= 0 {
-            dfmt.Printf("cb: %+v\n", cb)
-            dfmt.Printf("s: %+v m: %d", s, m)
-            cb_v, err := strconv.ParseFloat(s[1], 64)
-            if err == nil {
-                dfmt.Printf(" cb_v=%0.2f ", cb_v)
-                ss_m, err := strconv.ParseFloat(cb.Substrings[m], 64)
-                if err == nil {
-                    dfmt.Printf(" ss_m=%0.2f", ss_m)
-                    switch s[0] {
-                    case ">":  dfmt.Printf(" op=%s", s[0]); return matched(ss_m >  cb_v)
-                    case "<":  dfmt.Printf(" op=%s", s[0]); return matched(ss_m <  cb_v)
-                    case ">=": dfmt.Printf(" op=%s", s[0]); return matched(ss_m >= cb_v)
-                    case "<=": dfmt.Printf(" op=%s", s[0]); return matched(ss_m <= cb_v)
-                    }
-                }
-            }
-            dfmt.Printf("\n")
-        }
-    }
-    return 0;
 }
 
 func ProcessPatterns(args []string) []pattern {
